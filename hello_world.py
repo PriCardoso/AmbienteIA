a45e74e7934b359b964f32e9744517cec7594df1
Daniele Cardoso <danielecardoso@Danieles-MacBook-Air.local>
2024-01-26 12:47:51 -0300

    Comit Daniele

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+
+# Cria uma instância do Translator
+translator = Translator()
+
+# A frase que queremos traduzir
+phrase = "Olá Mundo"
+
+# Lista de idiomas para os quais queremos traduzir nossa frase.
+# Estes são os códigos de idioma para cada um dos idiomas.
+# Espanhol, Francês, Alemão, Russo e Chinês
+languages = ['es', 'fr', 'de', 'ru', 'zh-cn']
+
+# Traduzindo a frase para cada idioma
+for lang in languages:
+    translated_phrase = translator.translate(phrase, dest=lang)
+    print(f'{translated_phrase.origin} ({translated_phrase.src}) --> {translated_phrase.text} ({translated_phrase.dest})')
