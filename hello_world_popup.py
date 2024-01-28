4c3a089b1ab6885d545386a3a75873861532978f
PriCardoso <priscilaabcardoso@hotmail.com>
2024-01-28 12:34:59 -0300

    Banco de dados DAO

--- hello_world_popup.py
+++ hello_world_popup.py
@@ -1,47 +1,100 @@
 import tkinter as tk
 from tkinter import messagebox
+import sqlite3
 
-def exibir_popup(mensagem):
-    messagebox.showinfo("Pop-up", mensagem)
+        
+        
+class SaudacaoDAO:
+    def __init__(self):
+        self.conn = sqlite3.connect("saudacoes.db")
+        self.criar_tabela()
 
-def selecionar_idioma():
-    idioma_selecionado = var_idioma.get()
-    if idioma_selecionado == "Português":
-        exibir_popup("Olá, Mundo!")
-    elif idioma_selecionado == "Inglês":
-        exibir_popup("Hello, World!")
-    elif idioma_selecionado == "Espanhol":
-        exibir_popup("¡Hola, Mundo!")
-    elif idioma_selecionado == "Frances":
-        exibir_popup("Bonjour le monde!")
-    elif idioma_selecionado == "Alemão":
-        exibir_popup("Hallo Welt!")
-    elif idioma_selecionado == "Russo":
-        exibir_popup("Привет, мир!")
-    elif idioma_selecionado == "Chinês":
-        exibir_popup("你好世界!")
-    elif idioma_selecionado == "Finlandês":
-        exibir_popup("Hei maailma!")
-    elif idioma_selecionado == "Polonês":
-        exibir_popup("Witaj świecie!")
-    elif idioma_selecionado == "Hungaro":
-        exibir_popup("Helló Világ!")
+    def criar_tabela(self):
+        query = """CREATE TABLE IF NOT EXISTS saudacoes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    idioma TEXT,
+                    mensagem TEXT
+                )"""
+      
+        self.conn.execute(query)
+        self.conn.commit()
+        
+        # Adicione as saudações para os idiomas desejados
+        
+        idiomas_saudacoes = [
+            ("Português", "Olá, Mundo!"),
+            ("Inglês", "Hello, World!"),
+            ("Espanhol", "¡Hola, Mundo!"),
+            ("Frances", "Bonjour le monde!"),
+            ("Alemão", "Hallo Welt!"),
+            ("Russo", "Привет, мир!"),
+            ("Chinês", "你好世界!"),
+            ("Finlandês", "Hei maailma!"),
+            ("Polonês", "Witaj świecie!"),
+            ("Hungaro", "Helló Világ!"),
+            # Adicione mais idiomas e saudações conforme necessário
+            ]
+                           
+        for idioma, saudacao in idiomas_saudacoes:
+                self.adicionar_saudacao(idioma, saudacao)
+  
 
-# Criar a janela principal
-janela = tk.Tk()
-janela.title("Saudações Multilíngues")
+    def adicionar_saudacao(self, idioma, mensagem):
+        query = "INSERT INTO saudacoes (idioma, mensagem) VALUES (?, ?)"
+        self.conn.execute(query, (idioma, mensagem))
+        self.conn.commit()
 
-# Variável para armazenar a escolha do idioma
-var_idioma = tk.StringVar(janela)
-var_idioma.set("Português")  # Idioma padrão
+    def obter_saudacao(self, idioma):
+        query = "SELECT mensagem FROM saudacoes WHERE idioma = ?"
+        cursor = self.conn.execute(query, (idioma,))
+        resultado = cursor.fetchone()
+        return resultado[0] if resultado else None
 
-# Menu suspenso para selecionar o idioma
-menu_idioma = tk.OptionMenu(janela, var_idioma, "Português", "Inglês", "Espanhol", "Frabces", "Alemão", "Russo", "Chinês", "Finlandês", "Polonês", "Hungaro")
-menu_idioma.pack(pady=10)
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Saudações Multilíngues")
 
-# Botão para exibir a saudação
-botao_saudacao = tk.Button(janela, text="Mostrar Saudação", command=selecionar_idioma)
-botao_saudacao.pack(pady=20)
+        self.dao = SaudacaoDAO()
+    
+  
 
-# Iniciar o loop principal da janela
-janela.mainloop()
+        # Variável para armazenar a escolha do idioma
+        self.var_idioma = tk.StringVar(self.master)
+        self.var_idioma.set("Português")  # Idioma padrão
+        self.var_idioma.set("Inglês")
+        self.var_idioma.set("Espanhol")
+        self.var_idioma.set("Frances")
+        self.var_idioma.set("Alemão")
+        self.var_idioma.set("Russo")
+        self.var_idioma.set("Chinês")
+        self.var_idioma.set("Finlandês")
+        self.var_idioma.set("Polonês")
+        self.var_idioma.set("Hungaro")
+
+        # Menu suspenso para selecionar o idioma
+        
+        
+        self.menu_idioma = tk.OptionMenu(self.master, self.var_idioma, "Português", "Inglês", "Espanhol", "Frances", "Alemão", "Russo", "Chinês", "Finlandês", "Polonês", "Hungaro")
+        self.menu_idioma.pack(pady=10)
+            
+
+        # Botão para exibir a saudação
+        self.botao_saudacao = tk.Button(self.master, text="Mostrar Saudação", command=self.mostrar_saudacao)
+        self.botao_saudacao.pack(pady=20)
+        
+        
+
+
+    def mostrar_saudacao(self):
+        idioma_selecionado = self.var_idioma.get()
+        mensagem = self.dao.obter_saudacao(idioma_selecionado)
+        if mensagem:
+            messagebox.showinfo("Saudação", mensagem)
+        else:
+            messagebox.showwarning("Aviso", "Saudação não encontrada para o idioma selecionado.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
